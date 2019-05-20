35f3197a06687412bed2f9c8ca373bd4c2801301
Mostafa Aghajani <mostafa.aghajani@virta.global>
2019-05-20 15:01:57 +0300

    + object type map for different languages

--- SampleSetup/GenerateAll.sh
+++ SampleSetup/GenerateAll.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 dotnet ./Tools/ProtocolBuilder/pbuilder.dll ./Protocol/ -o ./Output/Swift/ -l swift
 dotnet ./Tools/ProtocolBuilder/pbuilder.dll ./Protocol/ -o ./Output/Kotlin/ -l kotlin
 dotnet ./Tools/ProtocolBuilder/pbuilder.dll ./Protocol/ -o ./Output/TypeScript/ -l typescript
-dotnet ./Tools/ProtocolBuilder/pbuilder.dll ./Protocol/ -o ./Output/PHP/ -l php -n 
+dotnet ./Tools/ProtocolBuilder/pbuilder.dll ./Protocol/ -o ./Output/PHP/ -l php -n ""
