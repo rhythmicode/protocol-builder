728c1931ef5bbc74764313ea213c7726d66ac0d9
Nikita Khlebushkin <nikita.khlebushkin@virta.global>
2019-08-20 12:14:31 +0300

    + change build directory for executable generation
    Former-commit-id: ba01f18095ff5d8c081dc615183d69107eeb5842

--- BuildExecutables.sh
+++ BuildExecutables.sh
@@ -1,14 +1,13 @@
 cd ProtocolBuilderCli
 
-dotnet publish -c release --runtime linux-x64 
-dotnet publish -c release --runtime win-x64  
+dotnet publish -c release --runtime linux-x64 --output ../tmp/linux
+dotnet publish -c release --runtime win-x64 --output ../tmp/windows
 
 cd ..
 curl -Lo warp-packer https://github.com/dgiagio/warp/releases/download/v0.3.0/linux-x64.warp-packer
 chmod +x warp-packer
 mkdir Executables
-./warp-packer --arch linux-x64 --input_dir ProtocolBuilderCli/bin/release/netcoreapp2.2/linux-x64/publish --exec pbuilder --output Executables/protocol-builder.sh
-./warp-packer --arch windows-x64 --input_dir ProtocolBuilderCli/bin/release/netcoreapp2.2/win-x64/publish --exec pbuilder.exe --output Executables/protocol-builder.exe
+./warp-packer --arch linux-x64 --input_dir tmp/linux --exec pbuilder --output Executables/protocol-builder.sh
+./warp-packer --arch windows-x64 --input_dir tmp/windows --exec pbuilder.exe --output Executables/protocol-builder.exe
 
-rm -rf ProtocolBuilderCli/bin
 rm -f warp-packer
