9eb9c6846ac9ee052156bd8752222bf6a6bdc5e6
Mostafa Aghajani <mostafa.aghajani@virta.global>
2019-09-10 18:43:37 +0300

    + Docker image build + Docker hub initial test + Sample setup to use docker and docker-compose

--- SampleSetup/GenerateAll.sh
+++
@@ -1,6 +0,0 @@
-cd "$(dirname "$0")"
-
-./tmp/tools/protocol-builder ./Protocol/ -o ./Output/Swift/ -l swift
-./tmp/tools/protocol-builder ./Protocol/ -o ./Output/Kotlin/ -l kotlin
-./tmp/tools/protocol-builder ./Protocol/ -o ./Output/TypeScript/ -l typescript
-./tmp/tools/protocol-builder ./Protocol/ -o ./Output/PHP/ -l php -n ""
---
+++ SampleSetup/docker-script.sh
@@ -0,0 +1,4 @@
+dotnet pbuilder.dll /source/ -o /output/Swift/ -l swift
+dotnet pbuilder.dll /source/ -o /output/Kotlin/ -l kotlin
+dotnet pbuilder.dll /source/ -o /output/TypeScript/ -l typescript
+dotnet pbuilder.dll /source/ -o /output/PHP/ -l php -n ""
